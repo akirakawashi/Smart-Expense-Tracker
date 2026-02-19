7bede65d8194dae2a75a76d99aa99cfa0f9e98b6
akirakawashi <mr.oldman.23@gmail.com>
2026-02-19 12:24:42 +0300

    Add UserModel for managing user data in the database

---
+++ src/infrastructure/database/models/users.py
@@ -0,0 +1,60 @@
+from datetime import datetime
+from typing import TYPE_CHECKING, Optional
+from sqlmodel import Field, Relationship, SQLModel
+from sqlalchemy import Column, DateTime
+from infrastructure.database.base import Base
+
+if TYPE_CHECKING:
+    from infrastructure.database.models.transaction import TransactionModel
+
+class UserModel(Base, table=True):
+    """
+    SQLModel for users table.
+    Maps to Domain Entity: User
+    """
+
+    __tablename__ = "users"
+
+    user_id: Optional[int] = Field(
+        default=None, 
+        primary_key=True, 
+        description="User ID"
+        )
+    
+    email: str = Field(
+        unique=True, 
+        nullable=False, 
+        description="User email"
+        )
+    
+    username: str = Field(
+        nullable=False, 
+        unique=False, 
+        description="Username"
+        )
+    
+    password: str = Field(
+        nullable=False, 
+        description="Hashed password"
+        )
+    
+    number: Optional[str] = Field(
+        default=None, 
+        description="Phone number"
+        )
+    
+    balance: float = Field(
+        default=0.0, 
+        description="User balance"
+        )
+    
+    date_created: datetime = Field(
+        default_factory=datetime.now, 
+        sa_column=Column(DateTime, default=datetime.now), 
+        description="Date created"
+        )
+
+    transactions: list["TransactionModel"] = Relationship(
+        back_populates="user",
+        sa_relationship_kwargs={"cascade": "all, delete-orphan"}
+    )
